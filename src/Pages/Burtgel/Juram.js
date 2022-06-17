5bb5e707f55ce6aa414fc67b2d1a3c84f4c3058f
Enkhtsolmon <enkh9933@gmail.com>
2022-06-17 13:56:18 +0700

    asdasd

--- src/Pages/Burtgel/Juram.js
+++ src/Pages/Burtgel/Juram.js
@@ -10,7 +10,7 @@ const Juram = () => {
         </Typography>
         <Box width={40} borderBottom={2} borderColor="#623CEA"></Box>{" "}
         <iframe
-          src="https://drive.google.com/file/d/1mwmGHwpklVPBGzaCMUbsnPn1B968QAlk/preview"
+          src="https://drive.google.com/file/d/1BUqelF_EzoAKTocTQ2A7LfSE0tEiMVhz/preview"
           width="100%"
           height="600px"
         ></iframe>
